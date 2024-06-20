e4998e4f7a908066d491f36b8ed300bff676c957
AbramBuck <82072155+AbramBuck@users.noreply.github.com>
2024-06-20 07:34:36 -0500

    problems 1 - 3 completed

--- aa01-basic-loops/problems/04-elements-of-odd-indices-reversed.js
+++ aa01-basic-loops/problems/04-elements-of-odd-indices-reversed.js
@@ -3,13 +3,14 @@ Implement the `elementsOfOddIndicesReversed` function so that it
 returns a new string containing all the elements with odd indices in the input
 string in reverse order.
 */
+//Function will return the entire string backwards. Trying to get it to only return the odd indicies.
 
 function elementsOfOddIndicesReversed(str) {
 
   let newStr = "";
 
   for (let i = str.length - 1; i >= 0; i-- ) {
-    if ( str[i] % 2 !== 0 ) {
+    if ( str[i] % 2 == 0 ) {
       newStr += str[i];
     }
   }
@@ -17,11 +18,12 @@ function elementsOfOddIndicesReversed(str) {
 } 
 
 console.log(elementsOfOddIndicesReversed("JollieBee")); //eilo
-console.log(elementsOfOddIndicesReversed("Hamburger Helper")); //rpe erba
-console.log(elementsOfOddIndicesReversed("SoundWire")); //eidno
-console.log(elementsOfOddIndicesReversed('academy')); // 'mdc'
-console.log(elementsOfOddIndicesReversed('planet'));  // 'tnl'
-console.log(elementsOfOddIndicesReversed('sport'));   // 'rp'
+
+//console.log(elementsOfOddIndicesReversed("Hamburger Helper")); //rpe erba
+//console.log(elementsOfOddIndicesReversed("SoundWire")); //eidno
+//console.log(elementsOfOddIndicesReversed('academy')); // 'mdc'
+//console.log(elementsOfOddIndicesReversed('planet'));  // 'tnl'
+//console.log(elementsOfOddIndicesReversed('sport'));   // 'rp'
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
